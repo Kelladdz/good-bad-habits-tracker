99ed9d3d908bf52bb6f8faa07039e928b7309707
Aleksander Kret <aleksander.kret1@gmail.com>
2023-11-20 17:12:31 +0100

    Creating a habit's model and DbContext classes

---
+++ GoodBadHabitsTracker/GoodBadHabitsTracker.Infrastructure/Persistance/HabitsDbContext.cs
@@ -0,0 +1,30 @@
+﻿using GoodBadHabitsTracker.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodBadHabitsTracker.Infrastructure.Persistance
+{
+    public class HabitsDbContext : DbContext
+    {
+        public HabitsDbContext(DbContextOptions<HabitsDbContext> options) : base(options)
+        {
+            
+        }
+
+        public HabitsDbContext()
+        {
+            
+        }
+
+        public DbSet<Habit> Habits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GoodBadHabitsTrackerDb;Trusted_Connection=True;");
+        }
+    }
+}
