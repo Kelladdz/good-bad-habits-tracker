0a5007421c839991dcdd1c546caccc98ab423f83
Aleksander Kret <aleksander.kret1@gmail.com>
2023-11-20 18:26:08 +0100

    Creating an validator for start date property in habit's class

---
+++ GoodBadHabitsTracker/GoodBadHabitsTracker.Core/Validators/MinimumDateValidator.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace good_bad_habits_tracker_api.Core.Validators
+{
+    public class MinimumDateValidator : ValidationAttribute
+    {
+        public DateOnly MinimumDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public string DefaultErrorMessage { get; set; } = "Start date should be in future or today.";
+
+
+        public MinimumDateValidator() { }
+
+        public MinimumDateValidator(DateOnly minimumDate)
+        {
+            MinimumDate = minimumDate;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateOnly date = (DateOnly)value;
+                if (date < MinimumDate)
+                {
+                    return new ValidationResult(string.Format(ErrorMessage ?? DefaultErrorMessage));
+                }
+                else
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return null;
+        }
+    }
+}
