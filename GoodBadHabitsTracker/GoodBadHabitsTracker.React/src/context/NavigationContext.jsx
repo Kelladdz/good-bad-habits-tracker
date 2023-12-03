daeb3d5e0355915062e9c2c725b5806ce46a6135
Aleksander Kret <aleksander.kret1@gmail.com>
2023-12-03 11:44:15 +0100

    Creating a navigation provider

---
+++ GoodBadHabitsTracker/GoodBadHabitsTracker.React/src/context/NavigationContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState, useEffect } from 'react';
+
+const NavigationContext = createContext();
+
+export default function NavigationProvider({ children }) {
+	const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+	useEffect(() => {
+		const handler = () => {
+			setCurrentPath(window.location.pathname);
+		};
+		window.addEventListener('popstate', handler);
+		return () => {
+			window.removeEventListener('popstate', handler);
+		};
+	}, []);
+
+	return <NavigationContext.Provider value={{}}>{children}</NavigationContext.Provider>;
+}
+
+export { NavigationProvider };
