b42acd64f09381f6a416d3c8d5b00578fddf475d
Aleksander Kret <aleksander.kret1@gmail.com>
2024-01-08 13:10:55 +0100

    Register method refactor

--- GoodBadHabitsTracker/GoodBadHabitsTracker.API/Controllers/v1/AuthController.cs
+++ GoodBadHabitsTracker/GoodBadHabitsTracker.API/Controllers/v1/AuthController.cs
@@ -28,18 +28,20 @@ namespace GoodBadHabitsTracker.API.Controllers.v1
         {
             try
             {
-                if (request == null) throw new NullReferenceException(nameof(request));
-                if (!ModelState.IsValid) throw new ArgumentException("User data is invalid");
+                if (request == null) throw new NullReferenceException("RequestDto cannot be null.");
+                if (!ModelState.IsValid) throw new ArgumentException("User data is invalid.");
 
                 var user = new ApplicationUser()
                 {
                     Email = request.Email,
                     UserName = request.Name,
                 };
+
                 IdentityResult result = await userManager.CreateAsync(user, request.Password!);
                 if (!result.Succeeded) throw new ConflictException("This name or email exists.", StatusCodes.Status409Conflict);
 
                 emailSender.SendWelcomeMessageAsync(user, user.Email);
+
                 return new CreatedAtRouteResult("GetUserById", new { userId = user.Id }, user);
             }
             catch (Exception ex)
@@ -56,17 +58,26 @@ namespace GoodBadHabitsTracker.API.Controllers.v1
         public async Task<IActionResult> Login
             ([FromBody] LoginDto request)
         {
-            var user = await userManager.FindByEmailAsync(request.Email);
-            if (user == null) return BadRequest();
-            var userName = user.UserName;
-            signInManager.AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            var result = await signInManager.PasswordSignInAsync(userName, request.Password, isPersistent: true, lockoutOnFailure: false);
+            try
+            {
+                if (request == null) throw new NullReferenceException(nameof(request));
+                if (!ModelState.IsValid) throw new ArgumentException(nameof(request));
+                var user = await userManager.FindByEmailAsync(request.Email);
+                if (user == null) throw new ArgumentException
+                var userName = user.UserName;
 
-            if (!result.Succeeded) return new UnauthorizedResult();
+                signInManager.AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                var result = await signInManager.PasswordSignInAsync(userName, request.Password, isPersistent: true, lockoutOnFailure: false);
+                if (!result.Succeeded) return new UnauthorizedResult();
 
-            Response.Cookies.Append("Logged", "true");
+                Response.Cookies.Append("Logged", "true");
 
-            return new OkResult();
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         [HttpGet("external-login")]
--- GoodBadHabitsTracker/GoodBadHabitsTracker.React/src/App.jsx
+++ GoodBadHabitsTracker/GoodBadHabitsTracker.React/src/App.jsx
@@ -20,8 +20,6 @@ function App() {
 
 	const { navigate } = useNavigation();
 
-	const clientId = '238617088969-sbq9rl49dhr623f55j6ae2c5g32r6sqk.apps.googleusercontent.com';
-
 	const googleLogin = async res => {
 		const email = res.profileObj.email;
 		const password = '';
